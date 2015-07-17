027281a48e4e12ecd2414f1e394789d0fafbc63c
Trang <trang@luunguyen.net>
2015-07-17 11:04:21 -0700

    Adding index page

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+    <title>Who's Who Add-in</title>
+</head>
+<body>
+    ##Index Page
+</body>
+</html> 
